a40a805b106c7c9fe541880131f5010ff68dcae8
xiinite <daemen.joey@gmail.com>
2016-05-02 10:20:21 +0200

    downtime minor change

--- controllers/downtimeController.js
+++ controllers/downtimeController.js
@@ -114,7 +114,7 @@ module.exports = {
         });
     },
     'submittedPeriods': function(req, res, next){
-        characterModel.find({googleId: req.user.googleId, state: {$in: ["Active"]}}, function (err, result) {
+        characterModel.find({googleId: req.user.googleId}, function (err, result) {
             if (err) return next(new Error(err));
             var characters = result;
             var characterids = result.map(function(item){
